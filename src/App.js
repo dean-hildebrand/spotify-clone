6a228714ea75d079ba56ff07b5a6957924b89ff3
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-11 09:13:25 -0500

    Use effect hook to check if a user has a token and then logs them in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import Login from './components/Login'
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { getTokenFromUrl } from "./spotify";
 
 // 0130b936efeb46c0bf49753f5acf0394
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  //runs code base on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("I have a token......", _token);
+  }, []);
+
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
