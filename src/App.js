835c01d8145c2545685ed6b77394bd44fe487f91
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-10 22:56:10 -0500

    Create login component and cleaned up react app unused files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Login from './components/Login'
+
+
+// 0130b936efeb46c0bf49753f5acf0394
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Spotify Clone</h1>
+
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/login.css'
+
+function Login() {
+  return (
+    <div className="login">
+    <h1>Login</h1>
+    {/* Spotify logo*/}
+    {/* Login with spotify button*/}
+    </div>
+  )
+}
+
+export default Login
