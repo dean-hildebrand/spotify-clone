f258094fe477673e0c5189c8d82dfb8945ec3d44
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-12 13:12:59 -0500

    Added material ui icons for playing songs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../css/footer.css'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutlineOutlined';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
 
 function Footer() {
   return (
     <div className="footer">
-    <h1>I am the footer component</h1>
+    <div className="footer__left">
+    <p>Album and song details</p>
+    </div>
+    <div className="footer__center">
+    <ShuffleIcon className="footer__green" />
+    <SkipPreviousIcon className="footer__icon" />
+    <PlayCircleOutlineIcon fontSize="large" className="footer__green" />
+    <SkipNextIcon className="footer__icon" />
+    <RepeatIcon className="footer__green" />
+
+    </div>
+    <div className="footer__right">
+    <p>Volume controls</p>
+
+    </div>
     </div>
   )
 }
