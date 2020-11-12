8cbd5ac25228339fc9d20080fc69673eb55394cf
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-12 12:33:00 -0500

    Added icons to sidebar with option text

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../css/sidebar.css'
 import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar() {
   return (
     <div className="sidebar">
     <img className="side__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"/>
-    <SidebarOption />
-    <SidebarOption />
-    <SidebarOption />
+    <SidebarOption title="Home" Icon={HomeIcon}/>
+    <SidebarOption title="Search" Icon={SearchIcon}/>
+    <SidebarOption title="Your Library" Icon={LibraryMusicIcon}/>
     </div>
   )
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import '../css/sidebarOption.css'
+import React from "react";
+import "../css/sidebarOption.css";
 
-function SidebarOption() {
+function SidebarOption({ title, Icon }) {
   return (
-    <div className="sidebar__option">
-    <h1>Sidebar Option</h1>
+    <div className="sidebarOption">
+      {Icon && <Icon className="sidebarOption__icon" />}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
     </div>
-  )
+  );
 }
 
-export default SidebarOption
+export default SidebarOption;
