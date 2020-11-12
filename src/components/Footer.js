980ba51622856d9da87d67b2ae2de40cf1d2b5ad
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-12 13:24:08 -0500

    Material UI grid container and slider for right side of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,42 @@
-import React from 'react'
-import '../css/footer.css'
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutlineOutlined';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import ShuffleIcon from '@material-ui/icons/Shuffle';
-import RepeatIcon from '@material-ui/icons/Repeat';
+import React from "react";
+import "../css/footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutlineOutlined";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayRoundedIcon from '@material-ui/icons/PlaylistPlayRounded';
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 function Footer() {
   return (
     <div className="footer">
-    <div className="footer__left">
-    <p>Album and song details</p>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__green" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+      <div className="footer__right">
+        {/*material ui grid container*/}
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayRoundedIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+            <Grid item xs></Grid>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
-    <div className="footer__center">
-    <ShuffleIcon className="footer__green" />
-    <SkipPreviousIcon className="footer__icon" />
-    <PlayCircleOutlineIcon fontSize="large" className="footer__green" />
-    <SkipNextIcon className="footer__icon" />
-    <RepeatIcon className="footer__green" />
-
-    </div>
-    <div className="footer__right">
-    <p>Volume controls</p>
-
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
