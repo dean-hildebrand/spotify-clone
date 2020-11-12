37ed87e4c2804c56f34823e9f9cb0918e0cf8381
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-12 12:18:16 -0500

    SidebarOption created and inserted into sidebar

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/sidebarOption.css'
+
+function SidebarOption() {
+  return (
+    <div className="sidebar__option">
+    <h1>Sidebar Option</h1>
+    </div>
+  )
+}
+
+export default SidebarOption
