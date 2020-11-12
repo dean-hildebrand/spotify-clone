e2a313daa5982c9cb795f6334953286205d93ee0
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-12 13:43:28 -0500

    Finished styling footer and aligning items and buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,23 @@ import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import { Grid, Slider } from "@material-ui/core";
-import PlaylistPlayRoundedIcon from '@material-ui/icons/PlaylistPlayRounded';
+import PlaylistPlayRoundedIcon from "@material-ui/icons/PlaylistPlayRounded";
 import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__left">
-        <p>Album and song details</p>
+        <img className="footer__albumLogo" src="https://upload.wikimedia.org/wikipedia/en/2/2f/Aenima.jpg" alt="" />
+        <div className="footer__songInfo">
+        <h4>Yeah!</h4>
+        <p>Usher</p>
+        </div>
       </div>
       <div className="footer__center">
         <ShuffleIcon className="footer__green" />
         <SkipPreviousIcon className="footer__icon" />
-        <PlayCircleOutlineIcon fontSize="large" className="footer__green" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
         <SkipNextIcon className="footer__icon" />
         <RepeatIcon className="footer__green" />
       </div>
