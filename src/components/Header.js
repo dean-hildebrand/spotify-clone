45de618764f13142cb155df67637c451e9585a6d
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-13 08:48:54 -0500

    Styled body banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "../css/header.css";
 import SearchIcon from "@material-ui/icons/Search";
-import { Avatar } from "@material-ui/core"
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "../DataLayer";
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+  console.log(user);
   return (
     <div className="header">
       <div className="header__left">
@@ -14,8 +17,8 @@ function Header() {
         />
       </div>
       <div className="header__right">
-      <Avatar src="" alt="DH"/>
-      <h4>Dean Hildebrand</h4>
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
