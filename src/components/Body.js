0cb1290c6ad0bf53c8a687acb3542d869cea566e
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-14 09:10:59 -0500

    inserted play and favorite icons and styled

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,9 @@ import React from "react";
 import "../css/body.css";
 import Header from "./Header";
 import { useDataLayerValue } from "../DataLayer";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled"
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
 
 
 function Body({ spotify }) {
@@ -21,6 +24,15 @@ function Body({ spotify }) {
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+
+      <div className="body__songs">
+      <div className="body__icons">
+      <PlayCircleFilledIcon  className="body__shuffle"/>
+      <FavoriteIcon fontSize="large"/>
+      <MoreHorizIcon />
+      </div>
+      {/* List of Songs */}
       </div>
     </div>
   );
